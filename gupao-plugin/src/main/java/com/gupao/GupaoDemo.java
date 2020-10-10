e79dfbc8534b31e13163749cad081100db309801
gks <gukeshuai@163.com>
2020-10-11 00:59:34 +0800

    gupao-plugin/.idea/compiler.xml

---
+++ gupao-plugin/src/main/java/com/gupao/GupaoDemo.java
@@ -0,0 +1,15 @@
+package com.gupao;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "gukeshuai",defaultPhase = LifecyclePhase.PACKAGE)
+public class GupaoDemo extends AbstractMojo{
+    private String aaa = "";
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        System.out.print("gupao plugin !!!!!!!!!!");
+    }
+}
